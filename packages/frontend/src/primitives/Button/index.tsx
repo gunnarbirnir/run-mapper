e6207e2f01048ede103fc7a6132357c5924e926b
Gunnar Birnir Ólafsson <gunnarbirnir@gmail.com>
2026-01-04 14:36:29 +0100

    Add Base UI

--- packages/frontend/src/primitives/Button/index.tsx
+++ packages/frontend/src/primitives/Button/index.tsx
@@ -1,26 +1,39 @@
+import type { ButtonHTMLAttributes } from 'react';
 import { Link } from '@tanstack/react-router';
+import { Button as BaseUiButton } from '@base-ui/react/button';
 
 import { cn } from '~/utils';
 
-import type { ButtonProps } from './types';
+type ButtonProps = {
+  children: string;
+  className?: string;
+  linkTo?: string;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
 const BASE_CLASS_NAME =
-  'inline-block px-4 py-2 bg-primary-500 text-white rounded hover:bg-primary-600';
+  'inline-block px-4 py-2 bg-primary-500 text-white rounded hover:bg-primary-600 cursor-pointer';
 
-export const Button = ({ children, className, ...props }: ButtonProps) => {
+export const Button = ({
+  children,
+  className,
+  linkTo,
+  ...props
+}: ButtonProps) => {
   const combinedClassName = cn(BASE_CLASS_NAME, className);
 
-  if ('linkTo' in props) {
+  if (linkTo) {
     return (
-      <Link to={props.linkTo} className={combinedClassName}>
-        {children}
+      <Link to={linkTo}>
+        <BaseUiButton {...props} className={combinedClassName}>
+          {children}
+        </BaseUiButton>
       </Link>
     );
   }
 
   return (
-    <button {...props} className={combinedClassName}>
+    <BaseUiButton {...props} className={combinedClassName}>
       {children}
-    </button>
+    </BaseUiButton>
   );
 };
