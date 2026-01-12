29c11778fc808c8bfa973f10a3c68d5e85116ea2
Gunnar Birnir Ólafsson <gunnarbirnir@gmail.com>
2026-01-12 13:10:21 +0100

    Update secondary color

--- packages/frontend/src/components/ElevationGraph/index.tsx
+++ packages/frontend/src/components/ElevationGraph/index.tsx
@@ -1,7 +1,7 @@
 import { useMemo } from 'react';
 import { CartesianGrid, Line, LineChart, XAxis, YAxis } from 'recharts';
 
-import { getCssVariableValue, convertRemToPixels } from '~/utils';
+import { getCssVariableValue, spacingPx } from '~/utils';
 import type { Elevation } from '~/types';
 
 import { processElevationData } from './utils';
@@ -15,14 +15,7 @@ const STROKE_WIDTH = 3;
 const AXIS_LINE_WIDTH = 1;
 
 export const ElevationGraph = ({ elevations }: ElevationGraphProps) => {
-  const lineColor = getCssVariableValue('--color-secondary-700');
-  const graphMargin = convertRemToPixels(getCssVariableValue('--spacing')) * 3;
-  const tickSpacing = convertRemToPixels(getCssVariableValue('--spacing'));
-  const xAxisHeight = convertRemToPixels(getCssVariableValue('--spacing')) * 5;
-  const yAxisWidth = convertRemToPixels(getCssVariableValue('--spacing')) * 10;
-  const xAxisTickGap =
-    convertRemToPixels(getCssVariableValue('--spacing')) * 10;
-  const yAxisTickGap = convertRemToPixels(getCssVariableValue('--spacing')) * 5;
+  const lineColor = getCssVariableValue('--color-secondary-500');
   const gridColor = getCssVariableValue('--color-gray-300');
   const textColor = getCssVariableValue('--color-gray-500');
   const xsText = getCssVariableValue('--text-xs');
@@ -36,11 +29,11 @@ export const ElevationGraph = ({ elevations }: ElevationGraphProps) => {
   return (
     <div style={{ height: GRAPH_HEIGHT }}>
       <LineChart
-        style={{ width: '100%', height: '100%', outline: 'none' }}
+        style={{ width: '100%', height: '100%' }}
         responsive
         data={elevationData}
         margin={{
-          top: graphMargin,
+          top: spacingPx(3),
           right: 1,
           bottom: 0,
           left: 0,
@@ -57,24 +50,24 @@ export const ElevationGraph = ({ elevations }: ElevationGraphProps) => {
         />
         <XAxis
           dataKey="distance"
-          height={xAxisHeight}
+          height={spacingPx(5)}
           axisLine={{ stroke: gridColor, strokeWidth: AXIS_LINE_WIDTH }}
           ticks={Array.from({ length: lastDistance }, (_, i) => i + 1)}
           tick={{ fill: textColor, fontSize: xsText }}
           tickFormatter={(value) => `${value.toFixed(1)} km`}
           tickLine={false}
-          tickMargin={tickSpacing}
-          minTickGap={xAxisTickGap}
+          tickMargin={spacingPx(1)}
+          minTickGap={spacingPx(10)}
         />
         <YAxis
           dataKey="value"
-          width={yAxisWidth}
+          width={spacingPx(10)}
           axisLine={{ stroke: gridColor, strokeWidth: AXIS_LINE_WIDTH }}
           tick={{ fill: textColor, fontSize: xsText }}
           tickFormatter={(value) => `${value.toFixed(0)} m`}
           tickLine={false}
-          tickMargin={tickSpacing}
-          minTickGap={yAxisTickGap}
+          tickMargin={spacingPx(1)}
+          minTickGap={spacingPx(5)}
         />
       </LineChart>
     </div>
