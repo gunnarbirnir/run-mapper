8af828ea29602b19908ac9fa1a18cfc9c25c6c52
Gunnar Birnir Ólafsson <gunnarbirnir@gmail.com>
2026-01-17 15:05:15 +0100

    Fix waypoint icons

--- packages/frontend/src/components/RouteMap/utils.ts
+++ packages/frontend/src/components/RouteMap/utils.ts
@@ -3,6 +3,7 @@ import type { Bounds, Coordinates, WayPointType } from '~/types';
 
 import type { LineFeature } from './types';
 import { BOUNDS_PADDING, LINE_WIDTH, LINE_OPACITY } from './constants';
+import { ENERGY_ICON, ENTERTAINMENT_ICON } from './icons';
 
 export const getPaddedBounds = (bounds: Bounds): Bounds => {
   const [minLng, minLat] = bounds[0];
@@ -62,24 +63,20 @@ export const getActiveMarkerElement = (): HTMLElement => {
   return marker;
 };
 
+const getIcon = (type: WayPointType) => {
+  switch (type) {
+    case 'energy':
+      return ENERGY_ICON;
+    case 'entertainment':
+      return ENTERTAINMENT_ICON;
+  }
+};
+
 export const getWaypointMarkerElement = (type: WayPointType): HTMLElement => {
   const marker = document.createElement('div');
   marker.className = `w-6 h-6 rounded-full border-3 border-white shadow-md/30 flex items-center justify-center`;
   marker.style.backgroundColor = getCssVariableValue('--color-secondary-500');
-  const iconColor = getCssVariableValue('--color-white');
-
-  const getIconPath = () => {
-    switch (type) {
-      case 'energy':
-        return 'M14.615 1.595a.75.75 0 0 1 .359.852L12.982 9.75h7.268a.75.75 0 0 1 .548 1.262l-10.5 11.25a.75.75 0 0 1-1.272-.71l1.992-7.302H3.75a.75.75 0 0 1-.548-1.262l10.5-11.25a.75.75 0 0 1 .913-.143Z';
-      case 'entertainment':
-        return 'M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z';
-    }
-  };
-
-  marker.innerHTML = `<svg viewBox="0 0 24 24" fill="${iconColor}" class="size-4">
-    <path fill-rule="evenodd" clip-rule="evenodd" d="${getIconPath()}" />
-  </svg>`;
+  marker.innerHTML = getIcon(type);
 
   return marker;
 };
