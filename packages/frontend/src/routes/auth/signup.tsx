9e431fe98bca6daff0d692d810bcd7c0819b21b9
Gunnar Birnir Ólafsson <gunnarbirnir@gmail.com>
2026-05-04 23:16:02 +0200

    Fix shader background down scaling

--- packages/frontend/src/routes/auth/signup.tsx
+++ packages/frontend/src/routes/auth/signup.tsx
@@ -12,10 +12,7 @@ function SignUp() {
   return (
     <PageLayout>
       <div className="flex-1 bg-white" />
-      <ShaderBackground
-        scaleDownByPixelRatio
-        className="absolute top-0 left-0 h-full w-full"
-      />
+      <ShaderBackground className="absolute top-0 left-0 h-full w-full" />
       <PageLayout.MainContent className="absolute top-0 left-0 flex h-full w-full items-center justify-center">
         <SignUpCard />
       </PageLayout.MainContent>
